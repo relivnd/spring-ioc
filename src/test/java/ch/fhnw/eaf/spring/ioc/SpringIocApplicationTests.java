38415b59b42d4cd6f2d766046f2950d70661d654
relivnd <tobiasvonesch@tobiass-mbp.edu.ds.fhnw.ch>
2022-09-19 09:23:31 +0200

    Initial commit & Solution

---
+++ src/test/java/ch/fhnw/eaf/spring/ioc/SpringIocApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.fhnw.eaf.spring.ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
