5c4ff26545898f6ffd750df6f5a267a367bfc85f
relivnd <hej+github@relivnd.dev>
2022-09-23 15:46:48 +0200

    Read "Hello World" from application.properties
    Signed-off-by: relivnd <hej+github@relivnd.dev>

--- src/main/java/ch/fhnw/eaf/spring/ioc/provider/HelloWorldProvider.java
+++ src/main/java/ch/fhnw/eaf/spring/ioc/provider/HelloWorldProvider.java
@@ -1,12 +1,17 @@
 package ch.fhnw.eaf.spring.ioc.provider;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Primary
 @Component
 public class HelloWorldProvider implements MessageProvider {
+
+    @Value("${string.message:Message Not Found}")
+    private String message;
+
     public String getMessage(){
-        return "Hello World";
+        return message;
     }
 }
